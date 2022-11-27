0c7feaefb592d4aa691c4b0c1ee254012fbdadf2
fabianmakila <contact@fabianadrian.fi>
2022-11-27 18:37:02 +0200

    Add nocollapse version

--- src/graphics/js/widget.ts
+++ src/graphics/js/widget.ts
@@ -2,49 +2,11 @@
 const text = document.getElementById("text")!;
 const textWrapper = document.getElementById("textWrapper")!;
 
-const texts = [
-  "WAEverything",
-  "discord.gg/wae"
-]
-
-const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
-
-let index = 0;
-
-async function nextText() {
-  expand();
-  await sleep(10000);
-  collapse();
-}
-
-async function expand() {
-  // Update text
-  text.textContent = texts[index];
-  if (index >= texts.length - 1) {
-    index = 0;
-  } else {
-    index = index + 1;
-  }
-
-  // Apply new width
-  const contentWidth = text.scrollWidth;
-  text.style.width = contentWidth + "px";
-  textWrapper.style.paddingInline = "1rem";
-  text.style.opacity = "1";
-}
-
-function collapse() {
-  text.style.opacity = "0";
-  text.style.width = "0px";
-  textWrapper.style.paddingInline = "0rem";
-}
-
-setInterval(nextText, 60000);
-
-
 // URL params
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
+
+const interval = 60000;
 
 switch (urlParams.get("position")) {
   case "topleft":
@@ -58,4 +20,99 @@ switch (urlParams.get("position")) {
     break;
   default:
     document.body.style.justifyContent = "right";
+}
+
+const collapseEnabled = !urlParams.has("nocollapse");
+let textWidth;
+
+// Temporary texts shown on widget
+// Will be moved to panel where they can be user configured
+const texts = [
+  "WAEverything",
+  "discord.gg/wae"
+]
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+let textIndex = 0;
+
+async function collapseRoutine() {
+  swapNextText();
+
+  const contentWidth = text.scrollWidth;
+  text.style.width = contentWidth + "px";
+  textWrapper.style.paddingInline = "1rem";
+
+  showText();
+
+  await sleep(10000);
+
+  // Collapse
+  hideText();
+  text.style.width = "0px";
+  textWrapper.style.paddingInline = "0rem";
+}
+
+function swapNextText() {
+  // Swap text
+  text.textContent = texts[textIndex];
+  if (textIndex >= texts.length - 1) {
+    textIndex = 0;
+  } else {
+    textIndex = textIndex + 1;
+  }
+}
+
+async function nonCollapseRoutine() {
+  hideText();
+
+  await sleep(2000);
+
+  // Store the current width before swapping text
+  const currentWidth = text.scrollWidth;
+
+  swapNextText();
+
+  // Remove transition temporally so we can get the new width
+  const transition = text.style.transition;
+  text.style.transition = "";
+  text.style.width = null;
+  // Grab the new width
+  const newWidth = text.scrollWidth;
+
+  requestAnimationFrame(function() {
+    // Apply old things back
+    text.style.width = currentWidth + 'px';
+    text.style.transition = transition;
+
+    // on the next frame (as soon as the previous style change has taken effect),
+    // have the element transition to the new width
+    requestAnimationFrame(function() {
+      text.style.width = newWidth + 'px';
+    });
+  });
+
+  showText();
+}
+
+function showText() {
+  text.style.opacity = "1";
+}
+
+function hideText() {
+  text.style.opacity = "0";
+}
+
+if (collapseEnabled) {
+  textWrapper.style.transition = "padding 2s";
+  text.style.width = "0px";
+  textWrapper.style.paddingInline = "0rem";
+
+  setInterval(collapseRoutine, interval);
+} else {
+  swapNextText();
+  textWrapper.style.paddingInline = "1rem";
+  showText();
+
+  setInterval(nonCollapseRoutine, interval);
 }
