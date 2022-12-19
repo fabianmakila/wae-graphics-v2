6a5c53e14cc169b8efd7cdeb31dcb5d803b422f1
fabianmakila <contact@fabianadrian.fi>
2022-12-20 01:49:13 +0200

    Use two.js for intermission background

--- src/graphics/js/intermission.ts
+++ src/graphics/js/intermission.ts
@@ -7,7 +7,6 @@ const logoImageElement: HTMLImageElement = document.getElementById("logo") as HT
 
 // Images
 const defaultLogo = new URL("../assets/wae-logo+wordmark.svg", import.meta.url);
-const barSvg = new URL("../assets/bar-6-units-default.svg", import.meta.url);
 
 // URL params
 const queryString = window.location.search;
@@ -24,110 +23,4 @@ logoReplicant.on("change", (newValue: any) => {
   }
 
   logoImageElement.src = newValue[0].url;
-})
-
-// Particles
-
-loadFull(tsParticles);
-
-tsParticles.load("particles1", {
-  detectRetina: true,
-  fpsLimit: 60,
-  particles: {
-    shape: {
-      type: "image",
-      image: {
-        src: barSvg.href,
-        height: 32,
-        width: 142,
-      }
-    },
-    move: {
-      direction: "left",
-      enable: true,
-      speed: 0.5,
-      straight: true
-    },
-    number: {
-      density: {
-        enable: true,
-        area: 500
-      },
-      value: 2
-    },
-    opacity: {
-      value: .5
-    },
-    size: {
-      value: 100
-    },
-  }
-});
-
-tsParticles.load("particles2", {
-  detectRetina: true,
-  fpsLimit: 60,
-  particles: {
-    shape: {
-      type: "image",
-      image: {
-        src: barSvg.href,
-        height: 32,
-        width: 142,
-      }
-    },
-    move: {
-      direction: "left",
-      enable: true,
-      speed: 0.75,
-      straight: true
-    },
-    number: {
-      density: {
-        enable: true,
-        area: 500
-      },
-      value: 1.5
-    },
-    opacity: {
-      value: .75
-    },
-    size: {
-      value: 150
-    },
-  }
-});
-
-tsParticles.load("particles3", {
-  detectRetina: true,
-  fpsLimit: 60,
-  particles: {
-    shape: {
-      type: "image",
-      image: {
-        src: barSvg.href,
-        height: 32,
-        width: 142,
-      }
-    },
-    move: {
-      direction: "left",
-      enable: true,
-      speed: 1,
-      straight: true
-    },
-    number: {
-      density: {
-        enable: true,
-        area: 500
-      },
-      value: 1
-    },
-    opacity: {
-      value: 1
-    },
-    size: {
-      value: 200
-    },
-  }
 });
